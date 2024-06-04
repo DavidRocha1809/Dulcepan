2e0ffdc2f01c003b8458572bbe053377e4089ab5
Joshua-SB <129015545+Joshua-SB@users.noreply.github.com>
2024-06-03 18:49:26 -0600

    Add files via upload

---
+++ htdocs/Prac/ArchivosPHP/login_usuario_be.php
@@ -0,0 +1,42 @@
+<?php
+
+    session_start();
+
+    include 'conexion_be.php';
+    
+    $Username = $_POST['Username'];
+    $Password = $_POST['Password'];
+
+
+    // Validar login de usuario
+    $validar_login = mysqli_query($conexion, "SELECT * FROM user WHERE Username='$Username' and Password='$Password'");
+
+    // Validar login de administrador
+    $validar_loginAd = mysqli_query($conexion, "SELECT * FROM admin WHERE Username='$Username' and Password='$Password'");
+
+    
+
+    if (mysqli_num_rows($validar_login) > 0) {
+        $_SESSION['Username'] = $Username;
+        $_SESSION['UserType'] = 'user'; // Guardar el tipo de usuario en la sesión
+        header("location: ../Index.html");
+        exit();
+    } elseif (mysqli_num_rows($validar_loginAd) > 0) {
+        $_SESSION['Username'] = $Username;
+        $_SESSION['UserType'] = 'admin'; // Guardar el tipo de usuario en la sesión
+        header("location: ../Index.html");
+        exit();
+    } else {
+        echo '
+            <script>
+                alert("El usuario o contraseña son incorrectos, por favor verifique los datos introducidos");
+                window.location = "../InicioSesion.php";
+            </script>
+        ';
+        exit();
+    }
+
+    
+
+
+?>
