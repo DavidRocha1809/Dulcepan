943c521ae4f3ebd7dd19e805bac6413cd7e6d2e1
DavidRocha1809 <92190796+DavidRocha1809@users.noreply.github.com>
2024-06-04 05:00:03 +0000

    prueba de index

---
+++ min.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.container-options span:first').addClass('active');
+    $('.container-products').hide();
+    $('.container-products:first').show();
+
+    $('.container-options span').click(function(){
+        $('.container-options span').removeClass('active');
+        $(this).addClass("active");
+        $('.container-products').hide();
+
+        var activeTab = $(this).data('target');
+        $(activeTab).show();
+        return false;
+        
+    })
+});
+
+btn.onclick = function() {
+    var buscar = document.getElementById("buscar").value;
+    if (buscar == 'hola'){
+        panel.innerHTML = "es una palabra española";
+    }
+}
